7647643e073b75fa7f746c700f51bbe6c0d2f021
Karson <kars.kalt@gmail.com>
2021-11-15 19:44:09 -0500

    Add array and reverseString.js

---
+++ algo_challenges/reverseString.js
@@ -0,0 +1,8 @@
+function reverse(str) {
+  // let stringBuilder = ""
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //     stringBuilder += str[i]
+  // }
+  // return stringBuilder
+  return str.split("").reverse().join("");
+}
