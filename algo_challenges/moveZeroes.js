089681f5074cf001fd9a6b49f3f5f3fde2867984
Karson <kars.kalt@gmail.com>
2021-11-19 11:01:34 -0500

    Move Zeroes complete.

--- algo_challenges/moveZeroes.js
+++ algo_challenges/moveZeroes.js
@@ -1,1 +1,18 @@
-function moveZeroes(nums) {}
+function moveZeroes(nums) {
+  let lengthToTraverse = nums.length - 1;
+  let currentIndex = 0;
+
+  while (currentIndex < lengthToTraverse) {
+    const currentNumber = nums[currentIndex];
+
+    if (currentNumber === 0) {
+      nums.splice(currentIndex, 1);
+      nums.push(0);
+      lengthToTraverse--;
+    } else {
+      currentIndex++;
+    }
+  }
+
+  return nums;
+}
