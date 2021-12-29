48ee13825b7cee584d75e420986dde54c3239e54
Karson <kars.kalt@gmail.com>
2021-12-29 08:17:40 -0500

    reconstructDigits with new var names

--- code_challenges/reconstructDigits.js
+++ code_challenges/reconstructDigits.js
@@ -1,55 +1,69 @@
 var originalDigits = function (s) {
-  nameMap = {
+  // let original = {
+  //     'zero': 0,
+  //     'one': 1,
+  //     'two': 2,
+  //     'three': 3,
+  //     'four': 4,
+  //     'five': 5,
+  //     'six': 6,
+  //     'seven': 7,
+  //     'eight': 8,
+  //     'nine': 9
+  // }
+
+  let keyMap = {
     z: 0,
-    o: 1,
+    o: 1, // - z - w - u
     w: 2,
-    h: 3,
+    h: 3, // - g
     u: 4,
-    f: 5,
+    f: 5, // - u
     x: 6,
-    s: 7,
+    s: 7, // - x
     g: 8,
-    i: 9,
+    i: 9, // - f - x - g
   };
 
-  hashMap = {
+  let countMap = {
     z: 0,
-    o: 0,
+    o: 0, // - z - w - u
     w: 0,
-    h: 0,
+    h: 0, // - g
     u: 0,
-    f: 0,
+    f: 0, // - u
     x: 0,
-    s: 0,
+    s: 0, // - x
     g: 0,
-    i: 0,
+    i: 0, // - f - x - g
   };
 
   let arr = s.split("");
+  console.log(arr);
 
+  // Initial count
   for (const letter of arr) {
-    if (letter in hashMap) {
-      hashMap[letter] = hashMap[letter] + 1;
+    if (letter in countMap) {
+      countMap[letter] = countMap[letter] + 1;
     }
   }
 
-  console.log(hashMap);
+  console.log(countMap);
 
-  // Subtract duplicates
-  hashMap["o"] = hashMap["o"] - hashMap["z"] - hashMap["w"] - hashMap["u"]; //one
-  hashMap["h"] = hashMap["h"] - hashMap["g"]; //three
-  hashMap["f"] = hashMap["f"] - hashMap["u"]; //five
-  hashMap["s"] = hashMap["s"] - hashMap["x"]; //seven
-  hashMap["i"] = hashMap["i"] - hashMap["f"] - hashMap["x"] - hashMap["g"]; //nine
+  // Adjustments for duplicate letters
+  countMap["h"] = countMap["h"] - countMap["g"]; // - g
+  countMap["f"] = countMap["f"] - countMap["u"]; // - u
+  countMap["s"] = countMap["s"] - countMap["x"]; // - x
+  countMap["o"] = countMap["o"] - countMap["z"] - countMap["w"] - countMap["u"]; // - z - w - u
+  countMap["i"] = countMap["i"] - countMap["f"] - countMap["x"] - countMap["g"]; // - f - x - g
 
-  console.log(hashMap);
+  console.log(countMap);
 
   let stringBuilder = [];
 
-  for (let k in hashMap) {
-    let count = hashMap[k];
-    for (i = 1; i <= count; i++) {
-      stringBuilder.push(nameMap[k]);
+  for (let letter in countMap) {
+    for (let i = 1; i <= countMap[letter]; i++) {
+      stringBuilder.push(keyMap[letter]);
     }
   }
 
